6784b26a63af2d39b54ea9b449982d5b39692c55
carsonlfreeman <freemancarson4@gmail.com>
2024-11-05 21:23:22 -0600

    Initial Commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.example.tankgame {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.tankgame to javafx.fxml;
+    exports com.example.tankgame;
+}
